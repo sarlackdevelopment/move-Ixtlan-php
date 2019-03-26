4696835b229d36f9f68886796006aa0827cbe843
Alex Trask <Sarlack_Work@mail.ru>
2019-03-26 14:09:26 +0300

    Добавил ReadBeanPHP!

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     include('src/Videomaker.php');
     include('src/Documenter.php');
 
+    include('src/controllers/Submitter.php');
+    $submitter   = new Submitter();
+
     $newser       = new Newser();
     $articler     = new Articler();
     $exhibitioner = new Exhibitioner();
@@ -89,9 +92,11 @@
         gtag('config', 'UA-130188408-1');
     </script>
     <!-- /Global site tag (gtag.js) - Google Analytics -->
+    <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>-->
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
@@ -177,6 +182,19 @@
                     <h4 class="text-center">Новости</h4>
 
                     <?php $newser->show_Main_Newses(); ?>
+
+                    <form action='/Ixtlan-php/src/controllers/work_with_ajax.php' method='post'>
+                        <div class="modal-body">                                   
+                            <label for="Caption">Заголовок новости</label>
+                            <textarea name='caption_news' class="form-control" rows="3"></textarea>
+
+                            <label for="Body">Текст новости</label>
+                            <textarea name='body_news' class="form-control" rows="3"></textarea>                                   
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-outline-primary btn-block my-1" type="submit">Добавить новость</button>
+                        </div>
+                    </form>
 
                     <div id="accordion">
 
