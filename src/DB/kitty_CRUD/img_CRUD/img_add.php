9f901c328860ff588fb6518361b5e673eba47e89
Alex Trask <Sarlack_Work@mail.ru>
2019-04-25 18:14:08 +0300

    Добавление фото в разрезе периодов

--- src/DB/kitty_CRUD/img_CRUD/img_add.php
+++ src/DB/kitty_CRUD/img_CRUD/img_add.php
@@ -6,32 +6,34 @@ R::setup( 'mysql:host=127.0.0.1;dbname=cats', 'root', '' );
 $post  = $_POST;
 $files = $_FILES;
 
-$log  = '/opt/lampp/htdocs/Ixtlan-php/debug.txt';
-$info = "kitty_id: " . $post['kitty_id'] . "; period_id: " . $post['period_id'];
-file_put_contents($log, $info, FILE_APPEND);
-
 /*********************************************************************************************************/
 /* Добавляем фото котенка */
 /*********************************************************************************************************/
 
- /* $kitty_id  = $post['kitty_id'];
+$kitty_id  = $post['kitty_id'];
 $period_id = $post['period_id'];
-$brood_id  = $post['brood_id'];
 
 $ds           = DIRECTORY_SEPARATOR; 
-$store_folder = $_SERVER['DOCUMENT_ROOT'] . '/Ixtlan-php/images/cats/kitty';
+$store_folder = $_SERVER['DOCUMENT_ROOT'] . '/Ixtlan-php';
 
 if (!empty($files)) {
 
-    if (isset($kitty_id) and isset($period_id) and isset($brood_id)) {
+    if (isset($kitty_id) and isset($period_id)) {
 
-        if (trim($kitty_id) != '' and trim($period_id) != '' and trim($brood_id) != '') {
+        if (trim($kitty_id) != '' and trim($period_id) != '') {
 
-            $brood  = R::findOne('broods', 'where id = ?', array($brood_id));
             $kitty  = R::findOne('kitty', 'where id = ?', array($kitty_id));
             $period = R::findOne('periods', 'where id = ?', array($period_id));
-            
-            $store_folder  = $store_folder . $ds . $brood['symbol'] . $ds . $kitty['name'] . $ds . $period['name'];
+
+            $array_path      = explode($ds, $kitty['main_photo']);
+            $array_path_tail = array_slice($array_path, 0, sizeof($array_path) - 1);
+            $path            = '';
+
+            foreach ($array_path_tail as $value) {
+                $path = $path . $ds . $value;
+            }
+
+            $store_folder = $store_folder . $ds . $path . $ds . $period['id'];
 
             if (!file_exists($store_folder)) {
                 mkdir($store_folder, 0777, true);
@@ -49,20 +51,19 @@ if (!empty($files)) {
             $img_kitty = R::dispense('imgkitty');
 
             $img_kitty->name = $file_name;
-            $img_kitty->path = 'images/cats/kitty/' . $brood['symbol'] . $ds . $kitty['name'] . $ds . $period['name'] . $ds . $file_name;
+            $img_kitty->path = $store_folder . $ds . $file_name;
 
             $img_kitty->sharedPeriodsList[] = $period;
 
             R::store($img_kitty);
 
-            // Связь один ко многим: помет-изображения котят 
-            //$brood->ownItemList[] = $img_kitty;
-            //R::store($brood);
+            $period->ownImgkittyList[] = $img_kitty;
+            R::store($period);
 
         }
 
     }
 
-} */
+}
 
 header("Location: /Ixtlan-php/kitty.php");
