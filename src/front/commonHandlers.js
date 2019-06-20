f8d1b617341a98ae2490c655e1e55870c581a895
Alex Trask <Sarlack_Work@mail.ru>
2019-06-20 14:10:21 +0300

    Отрефакторил глобальные обработчики

--- src/front/commonHandlers.js
+++ src/front/commonHandlers.js
@@ -1,29 +1,33 @@
 import MyResize from './adaptiveSize'
 import toggleNews from './toggleNews'
 
+const ready_JQuery_handler = () => {
+
+    MyResize()
+   
+    if (document.location.href.search('/comments/')) {
+        toggleNews(true)
+    }      
+
+}
+
+const load_Native_handler = () => {
+
+    let preloader = $('#p_prldr')
+    let svg_anm   = preloader.find('.svg_anm')
+
+    svg_anm.fadeOut()
+    preloader.delay(500).fadeOut('slow', () => preloader.removeClass("d-flex bd-highlight"))
+
+}
+
 const bindGlobalHandlers = () => {
 
-    $(() => {
+   $(() => ready_JQuery_handler())
 
-        MyResize()
-   
-       if (document.location.href.search('/comments/')) {
-           toggleNews(true)
-       }     
-   
-   }) 
+   $(window).on('resize', () => MyResize())
 
-   $(window).resize(() => MyResize())
-
-   $(window).on('load', () => {
-
-        let $preloader = $('#p_prldr')
-        let $svg_anm   = $preloader.find('.svg_anm')
-
-        $svg_anm.fadeOut()
-        $preloader.delay(500).fadeOut('slow', () => $preloader.removeClass("d-flex bd-highlight"))
-    
-    })
+   $(window).on('load', () => load_Native_handler())
 
 }
 
