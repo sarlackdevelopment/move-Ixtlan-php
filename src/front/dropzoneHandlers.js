776f6fee63edd80abf3e68549f9a222a3b27e010
Alex Trask <Sarlack_Work@mail.ru>
2019-06-26 12:36:07 +0300

    Решил проблему с двойным добавлением изображения

--- src/front/dropzoneHandlers.js
+++ src/front/dropzoneHandlers.js
@@ -6,7 +6,7 @@ const dropzoneHandlers = () => {
 
     for (let field_index = 1; field_index <= 4; field_index++) {
         Dropzone.options[`myDropzone${pagination_code}${field_index}`] = {
-            createImageThumbnails: false,
+            //createImageThumbnails: false,
             acceptedFiles: "image/*",
             maxFiles: 1,
             init: function() {
@@ -17,8 +17,14 @@ const dropzoneHandlers = () => {
                     formData.append("pagination_code", pagination_code)
                     formData.append("field_index", field_index)
                 })
+                this.on("addedfile", (file) => {
+                    if (!(file.initThumbnail) && (this.files[1] != null)) {
+                        this.removeFile(this.files[0]);
+                    }
+                })
+                execThumbnail(this)
 
-                let image = { name: "IMG_0045.JPG", size: 12345678, url: 'images/comments/1/4/IMG_0045.JPG' }
+                /* let image = { name: "IMG_0045.JPG", size: 12345678, url: 'images/comments/1/4/IMG_0045.JPG' }
 
                 let mockFile = {
                     name: image.name,
@@ -43,30 +49,41 @@ const dropzoneHandlers = () => {
                         this.emit('thumbnail', mockFile, thumbnail);
                         this.emit("complete", mockFile);
                     }
-                );
-                
-                /* let mockFile = { name: "IMG_0045.JPG", size: 12345678, imageUrl: 'images/comments/1/4/IMG_0045.JPG' }
-                this.emit("addedfile", mockFile);
-                this.files.push(mockFile);
-                this.createThumbnailFromUrl('images/comments/1/4/IMG_0045.JPG', mockFile.imageURL, () => this.emit("complete", mockFile), "anonymous");
-
-
-
-                let mockFile = { name: "IMG_0045.JPG", size: 12345678, imageUrl: 'images/comments/1/4/IMG_0045.JPG' }
-                this.files.push(mockFile);
-                this.emit("addedfile", mockFile);
-                this.createThumbnailFromUrl(mockFile,
-                    this.options.thumbnailWidth, 
-                    this.options.thumbnailHeight,
-                    this.options.thumbnailMethod, true, (thumbnail) => {
-                        this.emit('thumbnail', mockFile, thumbnail)
-                    })
-                this.emit('complete', mockFile); */
-
-
+                ) */
             }
         }
     }
+}
+
+const execThumbnail = (context) => {
+
+    let image = { name: "IMG_0045.JPG", size: 2321121, initThumbnail: true, url: 'images/comments/1/4/IMG_0045.JPG' }
+
+    let mockFile = {
+        name: image.name,
+        size: image.size,
+        accepted: true,
+        kind: 'image',
+        upload: {
+            filename: image.name,
+        },
+        dataURL: image.url,
+    };
+                
+    context.files.push(mockFile);
+    context.emit("addedfile", mockFile);
+    context.createThumbnailFromUrl(
+        mockFile,
+        context.options.thumbnailWidth,
+        context.options.thumbnailHeight,
+        context.options.thumbnailMethod,
+        context,
+        (thumbnail) => {
+            context.emit('thumbnail', mockFile, thumbnail);
+            context.emit("complete", mockFile);
+        }
+    )
+
 }
 
 export default dropzoneHandlers
