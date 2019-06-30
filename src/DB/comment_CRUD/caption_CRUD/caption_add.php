3ec0375d3642ea39d4f6f182645cc616d560e70a
Alex Trask <Sarlack_Work@mail.ru>
2019-06-30 20:56:35 +0300

    Починил баг с заголовками

--- src/DB/comment_CRUD/caption_CRUD/caption_add.php
+++ src/DB/comment_CRUD/caption_CRUD/caption_add.php
@@ -6,16 +6,16 @@ $json_obj = json_decode(file_get_contents('php://input'));
 
 $pagination_code = $json_obj->pagination_code;
 $field_index     = $json_obj->field_index;
-$caption_text    = $json_obj->caption_text;
+$current_text    = $json_obj->current_text;
 
-if ((isset($pagination_code)) and (isset($field_index)) and (isset($caption_text))) {
+if ((isset($pagination_code)) and (isset($field_index)) and (isset($current_text))) {
 
-    if ((trim($pagination_code) != '') and (trim($field_index) != '') and (trim($caption_text) != '')) {
+    if ((trim($pagination_code) != '') and (trim($field_index) != '') and (trim($current_text) != '')) {
         
         $current_comment = R::findOne('comments', 'where pagination_code = ?', array($pagination_code));
         $current_field   = 'caption' . $field_index;
 
-        $current_comment->$current_field = $caption_text;
+        $current_comment->$current_field = $current_text;
         R::store($current_comment);
 
     }
