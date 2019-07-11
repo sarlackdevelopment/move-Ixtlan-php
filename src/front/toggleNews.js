fd5116fd55f70d89f1239369aefb457b95b45f8e
Alex Trask <Sarlack_Work@mail.ru>
2019-07-11 14:33:09 +0300

    Удалил комментарии

--- src/front/toggleNews.js
+++ src/front/toggleNews.js
@@ -10,40 +10,12 @@ import {
     showNewsOnLGAsync 
 } from './utils/common.js'
 
-
-/* let $newsArea       = $("#newsArea")
-let $newsAreaBottom = $("#newsAreaBottom")
-let $mainNews       = $("#mainNews")
-let $mainArea       = $("#mainArea")
-
-
-
-export const hideNewsAsync = async () => {
-    await $newsArea.fadeOut("slow")
-    await $newsAreaBottom.fadeOut("slow")
-    await $mainNews.fadeOut("slow")
-}
-
-export const showNewsOnSMAsync = async () => {
-    await $newsArea.fadeOut("slow")
-    await $newsAreaBottom.fadeIn("slow")
-    await $mainNews.fadeIn("slow")
-}
-
-export const showNewsOnLGAsync = async () => {
-    await $newsArea.fadeIn("slow")
-    await $newsAreaBottom.fadeOut("slow")
-    await $mainNews.fadeOut("slow")
-} */
 let $mainArea = $("#mainArea")
 
 export const toggleNewsInit = () => {
 
     if (document.location.href.search('/comments') != -1) {
 
-        //$newsArea.hide()
-        //$newsAreaBottom.hide()
-        //$mainNews.hide()
         hideNews()
         $mainArea.attr("isPushed", "1")
         
@@ -52,17 +24,10 @@ export const toggleNewsInit = () => {
         if (window.matchMedia('(max-width: 768px)').matches) {
 
             showNewsOnSM()
-            //$newsArea.hide()
-            //$newsAreaBottom.show()
-            //$mainNews.show()
     
         } else {
 
             showNewsOnLG()
-    
-            //$newsArea.show()
-            //$newsAreaBottom.hide()
-            //$mainNews.hide()
     
         }
         $mainArea.attr("isPushed", "0")
@@ -79,17 +44,11 @@ export const toggleNews = async () => {
 
         if (!newsIsHidden) {
 
-            //await $newsArea.fadeOut("slow")
-            //await $newsAreaBottom.fadeOut("slow")
-            //await $mainNews.fadeOut("slow")
             await hideNewsAsync()
             $mainArea.attr("isPushed", "1")
 
         } else {
 
-            //await $newsArea.fadeOut("slow")
-            //await $newsAreaBottom.fadeIn("slow")
-            //await $mainNews.fadeIn("slow")
             await showNewsOnSMAsync()
             $mainArea.attr("isPushed", "0")
         }
@@ -98,18 +57,11 @@ export const toggleNews = async () => {
 
         if (!newsIsHidden) {
 
-            //await $newsArea.fadeOut("slow")
-            //await $newsAreaBottom.fadeOut("slow")
-            //await $mainNews.fadeOut("slow")
-
             await hideNewsAsync()
             $mainArea.attr("isPushed", "1")
 
         } else {
 
-            //await $newsArea.fadeIn("slow")
-            //await $newsAreaBottom.fadeOut("slow")
-            //await $mainNews.fadeOut("slow")
             await showNewsOnLGAsync()
             $mainArea.attr("isPushed", "0")
         }
